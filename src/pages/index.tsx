d70825f49e62cc8c13c199b50128facaf22fd81c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-14 18:53:06 -0300

    chore: add global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return [
     <Head>
       <title>Home | ig.news</title>
     </Head>,
-    <h1 className={styles.title}>
-      Next.<span>js</span>
+    <h1>
+      Next.js
     </h1>
   ]
 }
