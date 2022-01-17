957eb4a4f10d5d3cf29b15039c52329d248e6a70
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-17 07:15:54 -0300

    refactor: apply SSG to server side function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { stripe } from '../services/stripe'
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   ]
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IroO7HMEcBI9m1dJUutudS5', {
     expand: ['product'],
   })
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24,
   }
 }
