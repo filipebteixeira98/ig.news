6afef023d3f048d1267ddc59d5a3b0ea0221d8b7
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-01-20 05:57:27 -0300

    fix: added fragment wrapper element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,23 +15,25 @@ interface HomeProps {
 }
 
 export default function Home({ product }: HomeProps) {
-  return [
-    <Head>
-      <title>Home | ig.news</title>
-    </Head>,
-    <main className={styles.contentContainer}>
-      <section className={styles.hero}>
-        <span>👏 Hey, welcome</span>
-        <h1>News about the <span>React</span> world.</h1>
-        <p>
-          Get access to all the publications <br />
-          <span>for {product.amount} month</span>
-        </p>
-        <SubscribeButton priceId={product.priceId} />
-      </section>
-      <img src="/images/avatar.svg" alt="Girl coding"/>
-    </main>
-  ]
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
+    </>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
